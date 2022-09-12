188f67932d46a1fd67760ddcffc0fa5b7682a54f
Valentina <frankesteinblue@gmail.com>
2022-09-11 22:30:28 -0500

    back del proyecto completo

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoApi1.Models;
+
+namespace ProyectoApi1.Controllers
+{
+
+    [ApiController]
+    [Route("Client")]
+    public class ClientController: ControllerBase
+    {
+        [HttpGet]
+        [Route("list")]
+        public dynamic listClients()
+        {
+            List<Client> clients = new List<Client>
+            {
+                new Client
+                {
+                    id = 1,
+                    name = "Pepito Perez",
+                    email = "pepito@gmail.com",
+                    age = 19
+                },
+                new Client
+                {
+                    id = 2,
+                    name = "Clarita Oscuro",
+                    email = "clarita@gmail.com",
+                    age = 22
+                }
+            };
+            return clients;
+        }
+        [HttpGet]
+        [Route("listClient")]
+        public dynamic listClient(int id)
+        {
+
+            return new Client
+            {
+                id = id,
+                name = "Pepito Perez",
+                email = "pepito@gmail.com",
+                age = 19
+            };
+        }
+        [HttpPost]
+        [Route("save")]
+        public dynamic saveClient(Client client)
+        {
+            client.id = 3;
+            return new
+            {
+                success = true,
+                message = "Client insert",
+                result = client
+            };
+        }
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoApi1.Models
+{
+    public class Client
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int age   { get; set; }
+
+    }
+}
